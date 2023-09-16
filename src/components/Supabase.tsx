bbbf3dc7d323e01492027914c566040228dd05fa
Ed Bayes <51138168+ebayes@users.noreply.github.com>
2023-09-16 01:59:59 -0400

    update supabase

--- src/components/Supabase.tsx
+++ src/components/Supabase.tsx
@@ -5,7 +5,8 @@ import { convertName } from "@/components/convertName";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PRIVATE_KEY!,
+  // process.env.NEXT_PUBLIC_SUPABASE_PRIVATE_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
 
 async function getIpAddress() {
